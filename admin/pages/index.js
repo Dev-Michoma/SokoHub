768e7da0ee4942af3d3380b652ec73d238650e2d
Dev Michoma <codesplint151@gmail.com>
2024-12-01 01:42:57 +0300

    Ading the google login

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,13 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-
+import { useSession, signIn, signOut } from "next-auth/react"
 
 
 export default function Home() {
+  const { data: session } = useSession()
+  if(!session){
+    return 'not logged in'
+  }
   return (
-    <>
-     <div className='bg-red-500'>Test</div>
-    </>
+  <div className='bg-blue-900 h-screen w-scree flex items-center'       >
+    <div className='text-center w-full'>
+      <button className='bg-white p-2'> Login With Google</button>
+    </div>
+  </div>
   )
 }
