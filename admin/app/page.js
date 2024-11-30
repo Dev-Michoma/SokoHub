ed497d4843eb19e7666634f327ddb674a60654f1
Dev Michoma <codesplint151@gmail.com>
2024-11-30 22:05:36 +0300

    Addition of Login with google module

--- admin/app/page.js
+++ admin/app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-red-500">
-      Test
+    <div className="bg-blue-900 w-screen h-screen flex  items-center">
+      <div className="text-center w-full">
+      <button className="bg-white p-2 ">Login With Google</button>
+      </div>
     </div>
   );
 }
