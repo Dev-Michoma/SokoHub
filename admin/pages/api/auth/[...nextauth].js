1839f61748acd094ac98f47527ade1fa0571b6c5
Dev Michoma <codesplint151@gmail.com>
2024-12-02 00:03:11 +0300

    Creation of a mongoclient instance

--- admin/pages/api/auth/[...nextauth].js
+++ admin/pages/api/auth/[...nextauth].js
@@ -1,28 +1,34 @@
+import { MongoDBAdapter } from '@auth/mongodb-adapter'
+import { MongoClient } from 'mongodb'
 import NextAuth from 'next-auth'
-import AppleProvider from 'next-auth/providers/apple'
-import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from 'next-auth/providers/google'
-import EmailProvider from 'next-auth/providers/email'
+
+
+
+const mongoUri = process.env.MONGODB_URI;
+
+// Create a MongoClient instance
+const client = new MongoClient(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Define the clientPromise
+const clientPromise = client.connect();
+
+
+
 
 export default NextAuth({
   providers: [
     // OAuth authentication providers...
-    AppleProvider({
-      clientId: process.env.APPLE_ID,
-      clientSecret: process.env.APPLE_SECRET
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET
-    }),
+    
+   
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
-    // Passwordless / email sign in
-    EmailProvider({
-      server: process.env.MAIL_SERVER,
-      from: 'NextAuth.js <no-reply@example.com>'
-    }),
-  ]
+   
+  ] ,
+  adapter: MongoDBAdapter(clientPromise)
 })
--- admin/pages/index.js
+++ admin/pages/index.js
@@ -7,13 +7,17 @@ import { useSession, signIn, signOut } from "next-auth/react"
 export default function Home() {
   const { data: session } = useSession()
   if(!session){
-    return 'not logged in'
+    return(
+      <div className='bg-blue-900 h-screen w-scree flex items-center'       >
+    <div className='text-center w-full'>
+      <button onClick={() => signIn('google')} className='bg-white p-2'> Login With Google</button>
+    </div>
+  </div>
+    )
   }
   return (
-  <div className='bg-blue-900 h-screen w-scree flex items-center'       >
-    <div className='text-center w-full'>
-      <button className='bg-white p-2'> Login With Google</button>
-    </div>
+  <div>
+    Logged in {session.user.email}
   </div>
   )
 }
