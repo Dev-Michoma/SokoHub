bef2793941c05b1e14a3a322051477ba6dda93d4
Dev Michoma <codesplint151@gmail.com>
2024-12-02 03:19:51 +0300

    refactoring  the codebase and creating a Layout component

---
+++ admin/components/Layout.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+import { useSession, signIn, signOut } from "next-auth/react"
+import Nav from '@/components/Nav'
+
+export default function Layout() {
+  const { data: session } = useSession()
+  if(!session){
+    return(
+      <div className='bg-blue-900 h-screen w-screen flex items-center'       >
+    <div className='text-center w-full'>
+      <button onClick={() => signIn('google')} className='bg-white p-2'> Login With Google</button>
+    </div>
+  </div>
+    )
+  }
+  return (
+  <div className='bg-blue-900 h-screen flex  w-screen '>
+    <Nav/>
+  <div className='bg-white mt-1 mr-1 rounded-lg p-4 mb-2  flex-grow'>
+  {/* Logged in {session.user.email} */}
+  </div>
+  </div>
+  )
+}
+
+
+
--- admin/pages/index.js
+++ admin/pages/index.js
@@ -3,25 +3,15 @@ import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import { useSession, signIn, signOut } from "next-auth/react"
 import Nav from '@/components/Nav'
+import Layout from '@/components/Layout'
 
 export default function Home() {
-  const { data: session } = useSession()
-  if(!session){
+ 
     return(
-      <div className='bg-blue-900 h-screen w-screen flex items-center'       >
-    <div className='text-center w-full'>
-      <button onClick={() => signIn('google')} className='bg-white p-2'> Login With Google</button>
-    </div>
-  </div>
-    )
-  }
-  return (
-  <div className='bg-blue-900 h-screen w-screen '>
-    <Nav/>
-  <div className='text-white'>
-  {/* Logged in {session.user.email} */}
-  </div>
-  </div>
+   <Layout>
+
+   </Layout>
+ 
   )
 }
 
