07cee1e6572c7a340804c45148f40c857156534f
Dev Michoma <codesplint151@gmail.com>
2024-12-02 03:38:29 +0300

    Fixing the pathnames routing

--- admin/components/Nav.js
+++ admin/components/Nav.js
@@ -1,9 +1,15 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 
 export default function Nav(){
     const inactiveLink = 'flex gap-1 mt-2 ';
     const activeLink = inactiveLink + 'bg-white text-blue-900 rounded-l-lg';
+
+
+    const router = useRouter();
+    // console.log({router});
+    const {pathname} = router;
  return(
     <aside className="text-white pr-0">
     <a className="flex p-2 ">
@@ -16,7 +22,7 @@ export default function Nav(){
 
     </a>
  <nav className='p-2 mt-1'>
-    <Link className= {activeLink} href={'/'}>
+    <Link className= {pathname === '/' ? activeLink : inactiveLink} href={'/'}>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
     <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
      </svg>
@@ -24,7 +30,7 @@ export default function Nav(){
 
 
 
-    <Link className= {inactiveLink} href={'/products'}>
+    <Link className= {pathname.includes('/products') ? activeLink : inactiveLink} href={'/products'}>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
 </svg>
